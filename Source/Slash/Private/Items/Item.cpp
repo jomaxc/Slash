4f092b99d248aef150431b6a27eae8ee86eb0ef8
jomaxc <authorjomax@gmail.com>
2024-10-20 17:23:18 -0600

    Revert "Onscreen Debug Messages"
    This reverts commit 6bee641e

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -3,24 +3,22 @@
 
 #include "Items/Item.h"
 
+// Sets default values
 AItem::AItem()
 {
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
+// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
-	}
+	
 }
 
+// Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
