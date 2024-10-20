9ace7143816ec9a1910a480dd99da52f7396b17d
jomaxc <authorjomax@gmail.com>
2024-10-20 17:37:00 -0600

    Draw Debug Lines Corrected

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -23,18 +23,13 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-	}
-
 	UWorld* World = GetWorld();
+	
 	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+
 	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location + Forward * 100.f)
 
 }
 
